bd6f393ec5350015a36d589477abe975a07747e2
Goaloge <140845317+Goaloge@users.noreply.github.com>
2025-07-17 13:14:01 +0200

    Update sms_verification_admin_controller.rb

--- lib/admin/sms_verification_admin_controller.rb
+++ lib/admin/sms_verification_admin_controller.rb
@@ -1,7 +1,9 @@
 module Admin
   class SmsVerificationAdminController < ::Admin::AdminController
+    requires_plugin 'discourse-sms-verification'
+
     def index
-      render json: { success: true }
+      render json: { success: true, stats: {} } # Beispiel-Response
     end
   end
 end
